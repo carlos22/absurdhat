9221869cae5f5fa1103861c95b8d598c8462ef38
Karl Glatz <karl.glatz@rukzuk.com>
2014-02-18 14:49:14 +0100

    use submodule to build

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 var mixinAdapter = require('./mixinAdapter');
 function load(api, vendors) {
-	api.plugin('hatBackgroundImage', function (api, value) {return mixinAdapter(require('./mixins/background-image/background-image.js'), 'background-image', value, vendors);}); 
+	api.plugin('hatBackgroundImage', function (api, value) {return mixinAdapter(require('./lesshat/mixins/background-image/background-image.js'), 'background-image', value, vendors);}); 
 }
 
 module.exports = load;
